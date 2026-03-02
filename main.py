529cb71159779b525b7902f3258c899821014d91
mbutsk <mbutsk@icloud.com>
2026-03-02 22:13:00 +0300

    system remake

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import asyncio
 import logging
+import os
 
 from aiogram.methods.delete_webhook import DeleteWebhook
+from dotenv import load_dotenv
 
-from src import bot, dp
+from src import core
+
+load_dotenv()
+
+token = os.getenv("BOT_TOKEN")
+
+if token is None:
+    raise ValueError("BOT_TOKEN environment variable not set")
+
+mg = core.BotManager(token=token)
+
+bot = mg.bot
+dp = mg.dp
 
 
 async def main() -> None:
     await bot(DeleteWebhook(drop_pending_updates=True))
     logging.basicConfig(level=logging.INFO)
+    mg.load_routers()
     await dp.start_polling(bot)
 
 
